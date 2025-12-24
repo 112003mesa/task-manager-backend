d1ae827d10e49b6c62ae14374570b8b1d0ee67a1
Mesa <112003mesa@gmail.com>
2025-12-24 12:24:13 +0200

    Prepare Express backend for Vercel

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import corsOptions from "./config/corsOptions.js";
@@ -10,23 +10,22 @@ import "./models/TaskModel.js";
 import "./models/UserModel.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000
 
 const app = express();
+
 app.use(cors(corsOptions));
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 
+// connect DB
 connectDB();
 
-app.use("/api/user", userRoutes)
-app.use("/api/task", taskRoutes)
-
+// routes
+app.use("/api/user", userRoutes);
+app.use("/api/task", taskRoutes);
 
 app.get("/", (req, res) => {
-    res.send("API Running...");
-  });
+  res.send("API Running...");
+});
 
-app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`)
-})
+export default app;
